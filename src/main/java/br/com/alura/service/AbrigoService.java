c56b1be9072c282509e8a172c2a96ae0778303a6
Fabio Mendonça <fabio.mendonca@totvs.com.br>
2024-05-13 15:53:04 -0300

    refactor: protection in case of empty result

--- src/main/java/br/com/alura/service/AbrigoService.java
+++ src/main/java/br/com/alura/service/AbrigoService.java
@@ -3,10 +3,6 @@ package br.com.alura.service;
 import br.com.alura.client.ClientHttpConfiguration;
 import br.com.alura.domain.Abrigo;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
 
 import java.io.IOException;
 import java.net.http.HttpResponse;
@@ -50,6 +46,15 @@ public class AbrigoService {
         String responseBody = response.body();
         Abrigo[] abrigos = new ObjectMapper().readValue(responseBody, Abrigo[].class);
         List<Abrigo> abrigoList = Arrays.stream(abrigos).toList();
+
+        if (abrigoList.isEmpty()) {
+            System.out.println("Não há abrigos cadastrados!");
+        } else {
+            mostrarAbrigos(abrigoList);
+        }
+    }
+
+    private void mostrarAbrigos(List<Abrigo> abrigoList) {
         System.out.println("Abrigos cadastrados:");
         for (Abrigo abrigo : abrigoList) {
             long id = abrigo.getId();
